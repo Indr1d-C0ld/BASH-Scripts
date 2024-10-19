a988a1171496ba8add01ea62caa76795daa8b4b2
Indrid Cold <102763446+Indr1d-C0ld@users.noreply.github.com>
2024-10-19 15:41:37 +0200

    Create send_keogram.sh

---
+++ send_keogram.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Configurazione
+TOKEN="MY BOT TOKEN"
+CHANNEL_ID="CHANNEL ID"
+
+# Data di oggi in formato AAAAMMDD
+DATA=$(date +%Y%m%d)
+
+# Cartella contenente i file
+CARTELLA="/home/pi/allsky/images/$DATA/keogram"
+
+# Controlla se la cartella esiste
+if [ ! -d "$CARTELLA" ]; then
+    echo "La cartella $CARTELLA non esiste."
+    exit 1
+fi
+
+# Invia i file nella cartella
+for FILE in "$CARTELLA"/*; do
+    if [ -f "$FILE" ]; then
+        # Invia il file
+        curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendPhoto" -F "chat_id=$CHANNEL_ID" -F "photo=@$FILE"
+        echo "Inviato $FILE"
+    fi
+done
