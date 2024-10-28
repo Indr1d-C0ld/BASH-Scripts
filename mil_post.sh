8d3c91879b7b53e0112bf071581270afa283b1a2
Indrid Cold <102763446+Indr1d-C0ld@users.noreply.github.com>
2024-10-28 19:44:36 +0100

    Update mil_post.sh

--- mil_post.sh
+++ mil_post.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-BOT_TOKEN=""
-CHAT_ID=""
+BOT_TOKEN="7034351346:AAGp0J0rniIQWyrLjOVIhfCf-3YSiMLpd5s"
+CHAT_ID="-1002270663940"
 
 # Path del file JSON di dump1090
-JSON_FILE="/var/run/readsb/aircraft.json"
+JSON_FILE="/var/run/adsbfi-feed/aircraft.json"
+# JSON_FILE="/home/randolph/test.json"
 
 # Ottieni il volo militare più vicino
-NEAREST_MIL_FLIGHT=$(jq -r '.aircraft[] | select(.flight and (.flight | test("^(AME|UAF).*"))) | "Hex: #\(.hex); Flight: #\(.flight); Squawk: \(.squawk); Lat.: \(.lat); Lon.: \(.lon); Alt: \(.alt_baro)"' $JSON_FILE | head -n 1)
+NEAREST_MIL_FLIGHT=$(jq -r '.aircraft[] | select(.flight and (.flight | test("^(AME|ASY|BAF|BAH|BLUE|CEF|CFC|CMB|CTM|CNV|CPI|FAF|FLTCK|FMY|FNY|GAF|HAF|HUAF|IAF|IAM|KAF|LHOB|NAF|NATO|PAAF|PAT|PANTE|PLF|QID|RCH|RFF|ROF|RRF|RRR|RSF|SPHYR|SUI|TUAF|UAF).*"))) | "Hex: #\(.hex); Flight: #\(.flight); Squawk: \(.squawk); Alt.: \(.alt_baro)ft; Position: https://www.google.com/maps/@\(.lat),\(.lon),10z"' $JSON_FILE | head -n 1)
+# Lat.: \(.lat); Lon.: \(.lon);
 
 # Se non ci sono voli militari
 if [ -z "$NEAREST_MIL_FLIGHT" ]; then
